b024cc7fb2552e1e33638f3f73a274399be9e977
Alberto Sierra <alberto.sierra@murciaeduca.es>
2025-11-05 08:06:27 +0100

    Ejercicios de rutas resueltos

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,41 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Pantalla principal';
 });
+
+Route::get('/login', function () {
+    return 'Login usuario';
+});
+
+Route::get('/logout', function () {
+    return 'Logout usuario';
+});
+
+Route::prefix('/proyectos')->group(function () {
+    Route::get('/', function () {
+        return 'Listado proyectos';
+    });
+
+    Route::get('/show/{id}', function ($id) {
+        return "Vista detalle proyecto {$id}";
+    })->where('id', '[0-9]+');
+
+    Route::get('/create', function () {
+        return 'Añadir proyecto';
+    });
+
+    Route::get('/edit/{id}', function ($id) {
+        return "Modificar proyecto {$id}";
+    })->where('id', '[0-9]+');
+});
+
+Route::get('/perfil/{id?}', function ($id = null) {
+    if ($id) {
+        $message = "Visualizar el currículo de {$id}";
+    } else {
+        $message = "Visualizar el currículo propio";
+    }
+    return $message;
+})->where('id', '[0-9]+');
+
